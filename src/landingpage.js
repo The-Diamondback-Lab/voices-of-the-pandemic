c111c8dadb2acef10af23cf116f322a6de90fe19
Anthony Breder <awbreder@gmail.com>
2020-11-02 17:34:58 -0500

    sizing issues fixed

--- src/landingpage.js
+++ src/landingpage.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import landingpagebackground from './Assets/landingpage.jpg';
 import ScrollArrow from './ScrollArrow';
+import './landingpage.css';
 
 class LandingPage extends React.Component {
   render() {
     return (
-      <div>
-        <img style={{ width: '100%', height: '100%' }} src={landingpagebackground} />
-        <h1 style={{ position: 'absolute' , textAlign: 'center' , color: 'black' , left: '40%' , top: 300 }}>Voices of the Pandemic</h1>
-        <h2 style={{ position: 'absolute', textAlign: 'center', color: 'black', left: '27%', top: 350 }}>This is a blurb to go below the voices of the pandemic headline.</h2>
-        <h2 style={{ position: 'absolute', textAlign: 'center', color: 'black', left: '27%', top: 5000 }}>This is content for the page.</h2>
-        <ScrollArrow />
-      </div>
+      <React.Fragment>
+        <div className="header">
+          <h1 style={{ paddingTop: '22%', flex: '1' , textAlign: 'center' , fontSize: '2.5vw' }}>Voices of the Pandemic</h1>
+          <h2 style={{ paddingBottom: '22%', flex: '1' , textAlign: 'center' , fontSize: '2vw' }}>This is a blurb to go below the voices of the pandemic headline.</h2>
+          <ScrollArrow style={{ display: 'flex' }} />
+        </div>
+      </React.Fragment>
     );
   }
 }
